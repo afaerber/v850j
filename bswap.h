dc726d78fdbb0bda6501bc1ebe03b275b8a9e196
Andreas Färber <andreas.faerber@web.de>
2011-11-05 14:10:18 +0100

    Add byteswap support for Linux
    Signed-off-by: Andreas Färber <andreas.faerber@web.de>

--- bswap.h
+++ bswap.h
@@ -18,6 +18,20 @@
 #define be16_to_cpu(x) CFSwapInt16BigToHost(x)
 #define be32_to_cpu(x) CFSwapInt32BigToHost(x)
 
+#elif defined(__linux__)
+
+#include <byteswap.h>
+//#define __USE_BSD
+#include <endian.h>
+#define cpu_to_le16(x) htole16(x)
+#define cpu_to_le32(x) htole32(x)
+#define cpu_to_be16(x) htobe16(x)
+#define cpu_to_be32(x) htobe32(x)
+#define le16_to_cpu(x) le16toh(x)
+#define le32_to_cpu(x) le32toh(x)
+#define be16_to_cpu(x) be16toh(x)
+#define be32_to_cpu(x) be32toh(x)
+
 #else
 #error Byte swapping not yet implemented for this platform
 #endif
